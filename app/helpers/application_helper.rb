b92b396139808e0f980eda80493e6cbd2dfbda25
yanosz <yanosz@gmx.net>
2012-12-02 22:54:17 +0100

    Fix role symbols bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,9 @@ module ApplicationHelper
   def read_only(form)
     model = form.object
     method = form.options[:html][:method]
-    logger.debug "Role: #{current_user.role_symbols}"
     if(method == :put)
-      logger.debug "!#{model}.permitted_to? :update = #{!model.permitted_to? :update}"
       return(!model.permitted_to? :update)
     else
-      logger.debug "!#{model}.permitted_to? :create = #{!model.permitted_to? :create}"
       return(!model.permitted_to? :create)
     end
   end
