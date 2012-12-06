7b6b0bb6d4c94324a2ff2e951f453bb3f5c2452b
yanosz <yanosz@gmx.net>
2012-12-06 21:02:08 +0100

    Added additional header in mails

--- app/mailers/tinc_mailer.rb
+++ app/mailers/tinc_mailer.rb
@@ -5,18 +5,21 @@ class TincMailer < ActionMailer::Base
   def new_approved_email(user,node)
       @user = user
       @node  = node
+      headers 'X-KBU' => 'new_approved_email'
       mail(:to => user.email, :subject => "Node #{node.mac} ist nun Teil des Freifunk-Netzes")
   end
   
   def collision_info(user,node)
     @user = user
     @node = node
+    headers 'X-KBU' => 'collision_info'
     mail(:to => user.email, :subject => "Node #{node.mac}: Abweichender Key eingereicht")
   end
   
   def collision_resolve(user,node)
     @user = user
     @node = node
+    headers 'X-KBU' => 'collision_resolve'
     mail(:to => user.email, :subject => "Node #{node.mac}: VPN-Schlüssel muss bestätigt werden")
   end
 end
