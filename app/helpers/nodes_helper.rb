8d267685c3443f9cc674551f61fdac0fe65f9fc7
yanosz <yanosz@gmx.net>
2013-01-02 18:09:20 +0100

    Switch for fastd-graphs

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -1,2 +1,12 @@
 module NodesHelper
+
+  def ping_image_url(node)
+    url = 'http://kbu.freifunk.net/stats/ping/'
+    if(node.status.vpn_sw_name == "tinc")
+      url += 'ping_to_node-'
+    else
+      url += "ping_to_fastd_node-"
+    end
+    url += node.link_local_address + "_hour.png"
+  end
 end
