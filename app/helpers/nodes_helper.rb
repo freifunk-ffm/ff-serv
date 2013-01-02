85904aa3ff60705a6f1c0c7a9b569f5571eee8b7
yanosz <yanosz@gmx.net>
2013-01-02 18:46:57 +0100

    Added https for stats

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -1,7 +1,7 @@
 module NodesHelper
 
   def ping_image_url(node)
-    url = 'http://kbu.freifunk.net/stats/ping/'
+    url = 'https://kbu.freifunk.net/stats/ping/'
     if(node.status.vpn_sw_name == "tinc")
       url += 'ping_to_node-'
     else
