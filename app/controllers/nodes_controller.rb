dd14b77dcf24b8c01dad4a2c3ab640aa08ae0eed
yanosz <yanosz@gmx.net>
2013-03-10 22:05:14 +0100

    Renamed json api

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -10,7 +10,7 @@ class NodesController < ApplicationController
       format.json do
           resp = @registered.map do |node|
            reg = node.node_registration
-           {:vpn_status => {:name => node.status.vpn_status.name},
+           {:vpn_status => node.status.vpn_status.name,
              :status => {:created_at => I18n.l(node.status.created_at, :format => :short)},
              :node => {:id => node.id, :mac => node.mac, :mac_dec => node.mac.to_i(16), 
                :fw_version => ActionView::Base.full_sanitizer.sanitize(node.fw_version)},
