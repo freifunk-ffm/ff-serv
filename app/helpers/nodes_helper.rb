eb6a2ba153d6830f05536204711618650ea5f090
yanosz <yanosz@gmx.net>
2013-03-18 13:22:59 +0100

    Uses statistics from stats.kbu.freifunk.net

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -3,11 +3,11 @@ module NodesHelper
   def ping_image_url(node)
     url = 'https://kbu.freifunk.net/stats/ping/'
     if(node.status.vpn_sw_name == "tinc")
-      url += 'ping_to_node-' + node.link_local_address
+      url += 'ping_to_node-' + node.link_local_address + "_hour.png"
     else
-      url += "ping_to_fastd_node-" + node.link_local_address_short
+      #http://stat.kbu.freifunk.net/nodes/159387616943362/stats/ping.png?secs=3600&width=400&height=160&no_summary=1
+      url = "http://stat.kbu.freifunk.net/nodes/#{node.mac.to_i(16)}/stats/ping.png?secs=3600&width=400&height=160&no_summary=1"
     end
-    url += "_hour.png"
   end
 
   def thruput_image_url(node)
