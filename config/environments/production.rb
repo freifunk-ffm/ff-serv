4bd5df9e146a97f3169ef30271d8c572799c204c
Markus M. May <mmay@javafreedom.org>
2014-03-26 18:56:17 +0100

    Adopt changes done on live system

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,9 @@ Ffserv::Application.configure do
 
   ActionMailer::Base.delivery_method = :sendmail
 
-  config.to_prepare { Devise::SessionsController.force_ssl }
-  config.to_prepare { Devise::RegistrationsController.force_ssl  }
-  config.to_prepare { Devise::PasswordsController.force_ssl }
+#  config.to_prepare { Devise::SessionsController.force_ssl }
+#  config.to_prepare { Devise::RegistrationsController.force_ssl  }
+#  config.to_prepare { Devise::PasswordsController.force_ssl }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
