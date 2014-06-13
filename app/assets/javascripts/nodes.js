99dde77efc297b4b90ba85fc45d00be0c267fc4c
Markus M. May <triplem@javafreedom.org>
2014-06-13 21:45:49 +0200

    Adopt to use https urls for openstreetmap

--- app/assets/javascripts/nodes.js
+++ app/assets/javascripts/nodes.js
@@ -3,7 +3,7 @@ function init_node_map(nodes_map, lat, lng){
 	var map = L.map(nodes_map,{scrollWheelZoom: false}).setView([lat, lng], zoom);
 	L.marker([lat,lng],{icon: assetIcon}).addTo(map);
 	
-	var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+	var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 	L.tileLayer(osmUrl, {
 	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a></a>',
 	}).addTo(map);
